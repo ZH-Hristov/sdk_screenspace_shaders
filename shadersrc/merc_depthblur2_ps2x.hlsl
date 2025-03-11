c7e4bfd00403bccd0373f3b6e46ccd0ac3b23ab1
Mercury, 80Hg <zhhristov@abv.bg>
2025-03-11 14:34:47 +0200

    blur and depth blur

---
+++ shadersrc/merc_depthblur2_ps2x.hlsl
@@ -0,0 +1,37 @@
+#include "common.hlsl"
+
+float4 main( PS_INPUT i ) : COLOR
+{
+    float Pi = 6.28318530718; // Pi*2
+    
+    const float Directions = 16.0; // BLUR DIRECTIONS (Default 16.0 - More is better but slower)
+    const float Quality = 4.0; // BLUR QUALITY (Default 4.0 - More is better but slower)
+    float Size = Constants0.x; // BLUR SIZE (Radius)
+    float DepthStart = Constants0.y;
+    float DepthEnd = Constants0.z;
+
+    float Depth = tex2D( Tex1, i.uv ).r;
+   
+    float2 Radius = Size / TexBaseSize.xy;
+    Radius *= 0.0001;
+    Radius *= smoothstep( DepthStart, DepthEnd, Depth );
+    
+    // Normalized pixel coordinates (from 0 to 1)
+    float2 uv = i.uv;
+    // Pixel colour
+    float4 Color = tex2D(TexBase, uv);
+    
+    // Blur calculations
+    for( float d=0.0; d<Pi; d+=Pi/Directions)
+    {
+		for(float i=1.0/Quality; i<=1.0; i+=1.0/Quality)
+        {
+			Color += tex2D( TexBase, uv+float2(cos(d),sin(d))*Radius*i);		
+        }
+    }
+    
+    // Output to screen
+    Color /= Quality * Directions - 15.0;
+
+    return Color;
+}
